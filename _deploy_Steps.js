a71948f00de2bba7a8b7468d8648a15979ee3442
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-11-11 18:52:08 +0600

    added the search and sorting

---
+++ _deploy_Steps.js
@@ -0,0 +1,6 @@
+/* 
+1. add vercel.json config file
+2. command vercel in the cmd
+3. add npm run build to the package.json file ==> "build": "node index.js",
+4. set the env variables to the vercel website for the project which i want to deploy
+*/
--- index.js
+++ index.js
@@ -27,18 +27,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 //function for validate jwt token
-function verifyJWT (req, res, next) {
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     console.log(authHeader);
 
-    if(!authHeader){
-        return res.status(401).send({message: "Unauthorized Access"})
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access" })
     }
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded){
-        if(error){
-            return res.status(403).send({message: "Forbidden Access"})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+        if (error) {
+            return res.status(403).send({ message: "Forbidden Access" })
         }
 
         req.decoded = decoded;
@@ -57,28 +57,55 @@ async function run() {
 
         const orderCollection = client.db('geniusCar').collection('orders');
 
-        
-        
+
+
         //get all the services data from mongodb database server ==> Read Operation
-        app.get('/services', async(req, res) => {
-            const query = {};
-            const cursor = serviceCollection.find(query);
+        app.get('/services', async (req, res) => {
+
+            //query for price greater than 70 and less than 155
+            //const query = { price: { $gt: 70, $lt: 155} };
+
+            //filter the services by Ascending and descending order
+            const order = req.query.order === 'asc' ? 1 : -1;
+
+            const search = req.query.search;
+            console.log(search);
+
+
+            let query = {}
+
+            //client side e kono kichu search korle tar result er jonno code
+            if(search.length){
+                query = {
+                    $text: { $search: search }
+                };
+            }
+
+            // const query = {
+            //     $text: { $search: search }
+            // };
+
+            const cursor = serviceCollection.find(query).sort({ price: order });
             const services = await cursor.toArray();
             res.send(services);
         })
 
+
+
+
+
         //find a specific service data from mongodb server ==> Read Operation
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         })
 
 
-        
-        
-        
+
+
+
         // Client side er orders er jonno API create//
 
         // create (Means Add) orders data on database.. Client side theke checkout.js page er order data gula backend diye database e send kora. ==> Create operation
@@ -88,33 +115,26 @@ async function run() {
             res.send(result);
         })
 
-        // get all orders data from mongodb database ==> Read operation
-        // app.get('/orders', async(req, res) => {
-        //     const query = {};
-        //     const cursor = orderCollection.find(query);
-        //     const orders = await cursor.toArray();
-        //     res.send(orders);
-        // })
 
 
 
         // get an email specific order data from mongodb database ==> Read operation
-        app.get('/orders', verifyJWT, async(req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
             console.log(req.query.email);
-            
+
             //console.log(req.headers.authorization);
 
             const decoded = req.decoded;
             console.log(" Decoded inside Orders get API", decoded);
 
-            if(decoded.email !== req.query.email){
-                return res.status(403).send({message: "Forbidden Access"})
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ message: "Forbidden Access" })
             }
-            
+
 
             let query = {};
-            if(req.query.email){
-                query={
+            if (req.query.email) {
+                query = {
                     email: req.query.email
                 }
             }
@@ -124,24 +144,26 @@ async function run() {
             res.send(orders);
         })
 
+
+
         // delete an order data from mongodb database and also from client side ==> Delete operation
-        app.delete('/orders/:id', verifyJWT , async(req, res) => {
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
             res.send(result);
         })
 
         // update an order status from client side. Mane orders page er moddhe j order gula ache shei gular status ta change korbo.. Processing and approved ei vabe show korbe.. ==> Delete operation
-        app.patch('/orders/:id', verifyJWT, async(req, res) => {
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const status = req.body.status;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const updatedDoc = {
                 $set: {
                     status: status
                 }
-            } 
+            }
             const result = await orderCollection.updateOne(query, updatedDoc);
             res.send(result);
 
@@ -149,11 +171,11 @@ async function run() {
 
 
         //code for jwt token
-        app.post('/jwt', async(req, res) => {
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
             console.log("User From Sever side: ", user);
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '5h'} );
-            res.send({token})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
+            res.send({ token })
         })
 
 
